945e82f48c2faf7380041a615157f0bff3b6b483
camer0 <camero2734@outlook.com>
2018-08-04 02:41:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-var url_string = window.location.href
-var url = new URL(url_string);
-var c = url.searchParams.get("url");
-document.getElementsByTagName('head')[0].appendChild(`<meta property="og:video" content="${c}">`);
+// var url_string = window.location.href
+// var url = new URL(url_string);
+// var c = url.searchParams.get("url");
+// $('head').append(`<meta property="og:video" content="${c}">`);
+
+ var link=document.createElement('meta');
+ link.name="og:url";
+ link.content=document.location;
+ document.getElementsByTagName('head')[0].appendChild(link);
