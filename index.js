ecc3dbe8df3ded000a017e4fc8ed3145fa46f55b
camer0 <camero2734@outlook.com>
2018-08-16 12:16:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let url = 'https://library.iit.edu/hours'
 var el = document.createElement('html');
 Snekfetch.get("https://library.iit.edu/hours").then((r) => {
   el.innerHTML = r.body
-  let text = document.createElemnt("div")
+  let text = document.createElement("div")
   text.innerText = el.getElementsByClassName("clear")[0].children[0].innerText
   document.documentElement.appendChild(text)
 })
