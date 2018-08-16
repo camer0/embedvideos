8b809089ea093d0f5b37440bd1da17a30842223d
camer0 <camero2734@outlook.com>
2018-08-16 12:05:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,3 @@
 // h.innerText = "yeet"
 // document.documentElement.appendChild(h)
 let url = 'https://library.iit.edu/hours'
-
-$.ajax({
-  crossOrigin: true,
-  url: url,
-  success: function(data) {
-    alert('yeah')
-  }
-});
