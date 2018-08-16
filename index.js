3b19326c93387dd2969d0127464250c79ec55d2f
camer0 <camero2734@outlook.com>
2018-08-16 12:11:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 // h.innerText = "yeet"
 // document.documentElement.appendChild(h)
 let url = 'https://library.iit.edu/hours'
+var el = document.createElement( 'html' );
+Snekfetch.get("https://library.iit.edu/hours").then((r) => {
+  el.innerHTML = r.body
+  console.log(el.getElementsByTagName( 'a' ))
+})
