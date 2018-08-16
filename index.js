8e5351fec5555ba644ff9d0fc3e519d6d29cb8ef
camer0 <camero2734@outlook.com>
2018-08-16 12:15:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 // h.innerText = "yeet"
 // document.documentElement.appendChild(h)
 let url = 'https://library.iit.edu/hours'
-var el = document.createElement( 'html' );
+var el = document.createElement('html');
 Snekfetch.get("https://library.iit.edu/hours").then((r) => {
   el.innerHTML = r.body
-  console.log(el.getElementsByTagName( 'a' ))
+  let text = document.createElemnt("div")
+  text.innerText = el.getElementsByClassName("clear")[0].children[0].innerText
+  document.documentElement.appendChild(text)
 })
